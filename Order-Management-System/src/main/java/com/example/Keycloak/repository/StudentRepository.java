1074b130fea604e277664ad622eef4d8220507f2
Admin <vladimirnovik771@gmail.com>
2025-06-29 19:20:30 +0300

    Удалил лишний вложенный проект Order-Management-System

--- Order-Management-System/src/main/java/com/example/Keycloak/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Keycloak.repository;
-
-import com.example.Keycloak.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
